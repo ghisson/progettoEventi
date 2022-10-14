273fd3ee9ff99cdc19d8ecf2b0ab08c46d53af30
ghisson <simo.fortu@yahoo.it>
2022-10-14 17:49:04 +0200

    reso dinamito il form della prenotazione

--- src/app/ca-prenotazione/ca-prenotazione.component.ts
+++ src/app/ca-prenotazione/ca-prenotazione.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Settore } from '../model/settore';
@@ -12,22 +13,32 @@ import { ServiceUtente } from '../service/service-utente';
   styleUrls: ['./ca-prenotazione.component.css'],
 })
 export class CaPrenotazioneComponent implements OnInit {
+  dati: FormGroup;
   idSettoreDataEvento: any;
   idUtente: any;
   settoreDataEvento: SettoreDataEvento | undefined;
   countVal:number;
   val:number[];
+  codiciFiscali:any;
 
   constructor(
     private route: ActivatedRoute,
     private serviceUtente: ServiceUtente,
     private serviceSettoreDataEvento: ServiceEventoDataSettoreService,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder
   ) {
     this.countVal=0;
     this.idSettoreDataEvento = this.route.snapshot.paramMap.get('id');
     this.idUtente = this.serviceUtente.getId();
+
     this.val=[];
+
+    this.dati = this.fb.group({
+      
+    });
+    this.codiciFiscali=this.dati.get("codiciFiscali") as FormArray 
+
     this.serviceSettoreDataEvento
       .getSettoreDataEventoById(this.idSettoreDataEvento)
       .subscribe(
@@ -48,12 +59,26 @@ export class CaPrenotazioneComponent implements OnInit {
   aumenta():void {
     this.countVal++;
     this.val.push(this.countVal);
+    this.dati.addControl('codiFiscale'+this.countVal, new FormControl('', Validators.required));
+    
+
+
+ 
   }
   diminuisci():void {
     if(this.countVal>0){
       this.val.pop();
+      this.dati.removeControl('codiFiscale'+this.countVal)
       this.countVal--;
-    }
-    
+    } 
   }
+
+
+
+
+  invio(){
+    console.log(this.dati.value)
+  }
+
+
 }
